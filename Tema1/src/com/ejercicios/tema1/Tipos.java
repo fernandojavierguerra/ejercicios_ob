f9da3aacd430479a6040b118719d765e88ce9681
fernandojavierguerra <fernandojavierguerra@gmail.com>
2022-08-23 08:55:11 -0300

    Ejercicio 1

---
+++ Tema1/src/com/ejercicios/tema1/Tipos.java
@@ -0,0 +1,35 @@
+package com.ejercicios.tema1;
+
+public class Tipos {
+    public static void main(String[] args) {
+        System.out.println("Tipos de datos");
+        System.out.println("-------------------------------");
+
+        System.out.println("Enteros");
+        byte var_byte = 4;
+        System.out.println("byte: " + var_byte);
+        short var_short = 5;
+        System.out.println("short: " + var_short);
+        int var_int = 6;
+        System.out.println("int: " + var_int);
+        long var_long = 7;
+        System.out.println("long: " + var_long);
+        System.out.println("-------------------------------");
+
+        System.out.println("Decimales");
+        float var_float = 8f;
+        System.out.println("float: " + var_float);
+        double var_double = 9d;
+        System.out.println("double: " + var_double);
+        System.out.println("-------------------------------");
+
+        System.out.println("Caracter");
+        char var_char = 'z';
+        System.out.println("char: " + var_char);
+        System.out.println("-------------------------------");
+
+        System.out.println("Booleano");
+        boolean var_boolean = true;
+        System.out.println("boolean: " + var_boolean);
+    }
+}
