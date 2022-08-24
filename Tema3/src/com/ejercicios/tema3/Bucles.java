14353c985c0a4677746170d5cc0120cdd2750a73
fernandojavierguerra <fernandojavierguerra@gmail.com>
2022-08-24 12:39:45 -0300

    Ejercicio 3

--- Tema3/src/com/ejercicios/tema3/Bucles.java
+++ Tema3/src/com/ejercicios/tema3/Bucles.java
@@ -4,6 +4,15 @@ public class Bucles {
 
     public static void main(String[] args) {
 
+        String[] nombres = {"Fernando", "Javier", "Agustin", "Andres", "Liliana"};
+        String texto = "";
+
+        for (String nombre: nombres) {
+            texto = texto + nombre + " ";
+        }
+
+        System.out.println(texto);
+
     }
 
 
